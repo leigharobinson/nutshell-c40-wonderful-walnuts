a6c8b9c94d304594d09fcb5592887dce1e6468bb
Adrian Garmendia <dejima@gmail.com>
2020-05-20 19:42:04 -0500

    api calls set up

--- src/scripts/chat/chatData.js
+++ src/scripts/chat/chatData.js
@@ -3,14 +3,15 @@ let chat = [];
 const getChat = () => {
 	return fetch("http://localhost:8088/messages")
 		.then((res) => res.json())
-    .then((parsedChat) => (chat = parsedChat));
-}
-    
-    const editChat = () => {
-   return fetch("http://localhost:8088/messages"
-   method: "PUT",
-   headers: {
-     "Content-Type": "application/json",
-   },
-   body: JSON.stringify() 
-    }
+		.then((parsedChat) => (chat = parsedChat));
+};
+
+const editChat = () => {
+	return fetch("http://localhost:8088/messages", {
+		method: "PUT",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(),
+	});
+};
